2ef9c9aed8682e691ff9e22ddd8bf3b8498771d8
Roumen Damianoff <roumen@dawebs.com>
2015-11-06 11:12:33 +0300

    added emails views

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>{{ $name }}: {{ $subject }}</h2>
+
+		<div>
+			{!! $body !!}
+		</div>
+	</body>
+</html>
