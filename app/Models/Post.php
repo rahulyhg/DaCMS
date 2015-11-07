3c0c2416bad4cab81df9eefa80839aa2084e935c
Roumen Damianoff <roumen@dawebs.com>
2015-11-07 11:15:33 +0300

    added Post model

---
+++ app/Models/Post.php
@@ -0,0 +1,38 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'tags_to_posts', 'posts_slug', 'tags_slug');
+
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category', 'categories_to_posts', 'post_id', 'category_id');
+
+    }
+
+    public function user()
+    {
+        $this->primaryKey = 'id';
+        return $this->belongsTo('App\User');
+    }
+
+
+    public function lcomments()
+    {
+        return $this->hasMany('App\Comment','slug');
+    }
+
+
+}
