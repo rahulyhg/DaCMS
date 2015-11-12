b25177c9cd6838ad14286f14d3cbdf3ecd7e3184
Roumen Damianoff <roumen@dawebs.com>
2015-11-12 11:15:33 +0300

    added Tag model

---
+++ app/Models/Tag.php
@@ -0,0 +1,22 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Config;
+
+class Tag extends Model
+{
+
+
+    protected $table = 'tags';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+
+    public function posts()
+    {
+      return $this->belongsToMany('App\Post', 'tags_to_posts', 'tag_id', 'post_id')->whereIn('isVisible', array('1','2'))->orderBy('created_at','desc');
+    }
+
+
+
+}
