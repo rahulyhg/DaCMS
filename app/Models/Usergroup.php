6b028491dac48c4858f110d5dea13227d18f814f
Roumen Damianoff <roumen@dawebs.com>
2015-11-12 11:26:33 +0300

    added Usergroup model

---
+++ app/Models/Usergroup.php
@@ -0,0 +1,19 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usergroup extends Model
+{
+
+    protected $table = 'usergroups';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+
+    public function users()
+    {
+      return $this->belongsToMany('App\User', 'users_to_usergroups', 'usergroup_id', 'user_id');
+    }
+
+
+}
