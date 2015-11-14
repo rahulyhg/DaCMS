7cfda21982831a04cc309c53ca39b8f4ec704184
Roumen Damianoff <roumen@dawebs.com>
2015-11-14 11:21:33 +0300

    updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,26 +10,17 @@ Route::group(array('domain' => env('APP_DOMAIN')), function()
 {
 
 // HomeController
-Route::get('/', 'HomeController@getIndex');
 Route::get('contact', 'HomeController@getContact');
 Route::post('contact', 'HomeController@postContact');
+Route::get('feed', 'HomeController@getFeed');
+Route::get('sitemap', 'HomeController@getSitemap');
+
+// UserCotroller
 Route::get('login', 'HomeController@getLogin');
 Route::post('login', 'HomeController@postLogin');
 Route::get('logout', 'HomeController@getLogout');
 Route::get('user/{id}', 'HomeController@getUser');
-Route::get('feed', 'HomeController@getFeed');
-Route::get('feed/projects', 'HomeController@getFeedprojects');
-Route::get('sitemap', 'HomeController@getSitemap');
-
-// PageController
-Route::get('about', 'PageController@getAbout');
-Route::get('privacy', 'PageController@getPrivacy');
-Route::get('page/add', 'PageController@getCreate');
-Route::post('page/add', 'PageController@postCreate');
-Route::get('page/edit/{id}', 'PageController@getEdit');
-Route::post('page/edit/{id}', 'PageController@postEdit');
-Route::get('page/del/{id}', 'PageController@getDelete');
-Route::post('page/del/{id}', 'PageController@postDelete');
+Route::get('dashboard', 'HomeController@getDashboard');
 
 // PostController
 Route::get('blog', 'PostController@getIndex');
@@ -42,7 +33,7 @@ Route::post('blog/del/{id}', 'PostController@postDelete');
 Route::get('blog/{slug}', 'PostController@getView');
 
 // CategoryController
-Route::get('category', 'CategoryController@getIndex');
+Route::get('categories', 'CategoryController@getIndex');
 Route::get('category/add', 'CategoryController@getCreate');
 Route::post('category/add', 'CategoryController@postCreate');
 Route::get('category/edit/{id}', 'CategoryController@getEdit');
@@ -52,18 +43,17 @@ Route::post('category/del/{id}', 'CategoryController@postDelete');
 Route::get('category/{slug}', 'CategoryController@getView');
 
 // TagController
-Route::get('tag', 'TagController@getIndex');
+Route::get('tags', 'TagController@getIndex');
 Route::get('tag/{slug}', 'TagController@getView');
 
-// AdminController
-Route::get('admin', 'AdminController@getIndex');
-Route::get('admin/sync', 'AdminController@getSync');
-Route::get('admin/update', 'AdminController@getUpdate');
-
-});
-
-// cdn.dacms.dev
-Route::group(array('domain' => 'cdn.'.env('APP_DOMAIN')), function()
-{
-    Route::get('/', 'CDN\CDNController@getIndex');
+// PageController
+Route::get('page/add', 'PageController@getCreate');
+Route::post('page/add', 'PageController@postCreate');
+Route::get('page/edit/{id}', 'PageController@getEdit');
+Route::post('page/edit/{id}', 'PageController@postEdit');
+Route::get('page/del/{id}', 'PageController@getDelete');
+Route::post('page/del/{id}', 'PageController@postDelete');
+Route::get('/pages', 'PageController@getIndex');
+Route::get('/', 'PageController@getView');
+Route::get('/{slug}', 'PageController@getView');
 });
