3d63ed96b194cada17fd9e2fc59ba860d3f25d47
Roumen Damianoff <roumen@dawebs.com>
2015-11-17 11:23:37 +0300

    updated PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,15 +14,16 @@ class PageController extends Controller
 
     public function __construct()
     {
-        //$this->middleware('auth', ['except' => ['getAbout','getPrivacy']]);
-        //$this->middleware('admin', ['except' => ['getAbout','getPrivacy']]);
+        $this->middleware('auth', ['except' => ['getIndex','getView']]);
+        $this->middleware('editor', ['except' => ['getIndex','getView','getDelete','postDelete']]);
+        $this->middleware('moderator', ['only' => ['getDelete','postDelete']]);
     }
 
 
 
     public function getIndex()
     {
-        //
+        // TODO
     }
 
 
@@ -42,13 +43,18 @@ class PageController extends Controller
     {
         $page = Page::where('id','=',$id)->first();
 
-        $meta['title'] = 'Редактирай: '.$page->title.' | Roumen.IT';
+        $meta['title'] = 'Edit page';
 
-        Asset::add('https://cdn.roumen.it/repo/ckeditor/ckeditor.js');
-        Asset::add(secure_url('/js/page_edit.js'));
+        $s1 = "$(function(){CKEDITOR.replace('page_content',{toolbar:'Standart',height:'300px',width:'760px',toolbarCanCollapse:true,toolbarStartupExpanded:true,startupShowBorders:true});});";
+        $s2 = "$('#deleteBtn').click(function(){window.location = '".secure_url('/page/del/'.$id)."';});";
+
+        Asset::add('https://cdn.roumen.it/repo/ckeditor/4.4.6/basic/ckeditor.js');
+        Asset::addScript($s1, 'ready');
+        Asset::addScript($s2, 'ready');
 
         return view('page.edit')->with('meta', $meta)->with('page', $page);
     }
+
 
 
     public function postEdit($id)
@@ -72,8 +78,7 @@ class PageController extends Controller
                    'description' => Input::get('description'),
                    'keywords' => Input::get('keywords'),
                    'robots' => Input::get('robots'),
-                   'lang' => Input::get('lang'),
-                   'modified' => date('Y-m-d H:i:s')
+                   'lang' => Input::get('lang')
                 );
 
          Page::where('id','=',$id)->update($data);
@@ -84,45 +89,79 @@ class PageController extends Controller
     }
 
 
+
     public function getCreate()
     {
         $meta['title'] = "Create page";
 
-        Asset::add('https://cdn.roumen.it/repo/ckeditor/ckeditor.js');
-        Asset::add(secure_url('/js/page_edit.js'));
+        Asset::add('https://cdn.roumen.it/repo/ckeditor/4.4.6/basic/ckeditor.js');
+        $script = "$(function(){CKEDITOR.replace('page_content',{toolbar:'Standart',height:'300px',width:'760px',toolbarCanCollapse:true,toolbarStartupExpanded:true,startupShowBorders:true});});";
+        Asset::addScript($script, 'ready');
 
         return view('page.create')->with('meta', $meta);
     }
 
 
+
     public function postCreate()
     {
-        // todo
+        $rules = array(
+                'title'  => 'required|min:3|max:80',
+                'slug' => 'required|min:3',
+                'page_content'  => 'required|min:15',
+                'description'  => 'required|min:10|max:180',
+                'keywords' => 'required|min:3|max:90'
+        );
+
+        $validation = Validator::make(Input::all(), $rules);
+
+        if ($validation->passes())
+        {
+                $data = array(
+                   'title' => Input::get('title'),
+                   'slug' => Input::get('slug'),
+                   'content' => Input::get('page_content'),
+                   'description' => Input::get('description'),
+                   'keywords' => Input::get('keywords'),
+                   'robots' => Input::get('robots'),
+                   'lang' => Input::get('lang'),
+                   'user_id' => Auth::user()->id
+                );
+
+         Page::insert($data);
+
+         return Redirect::secure('/'.Input::get('slug'));
+
+        }
+
+        return Redirect::secure('page/add')->withErrors($validation);
     }
+
 
 
     public function getDelete($id)
     {
-        $page = Page::where_id($id)->first();
+        $page = Page::where('id','=',$id)->first();
 
-        $meta['title'] = 'DELETE: ' . $page->title .' | Roumen.IT';
-        $meta['canonical'] = 'http://roumen.it/page/'.$page->slug;
+        $meta['title'] = 'DELETE: ' . $page->title;
 
-        return view('page.delete')->with('meta', $meta)->with('page', $page->to_array());
+        return view('page.delete')->with('meta', $meta)->with('page', $page);
     }
+
 
 
     public function postDelete($id)
     {
           if (Input::get('confirm')==true)
           {
-              Page::where_id($id)->delete();
+              Page::where('id','=',$id)->delete();
 
               return Redirect::secure('/');
           }
 
           return Redirect::secure('page/del/'.$id);
     }
+
 
 
 }
