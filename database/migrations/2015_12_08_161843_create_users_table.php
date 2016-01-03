ffb83e38aee3be2bc6bdba1e1ad2205f3bc10973
Roumen Damianoff <roumen@dawebs.com>
2016-01-03 11:19:45 +0300

    added create_users_table migration

---
+++ database/migrations/2015_12_08_161843_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('username', 20)->unique('username');
+			$table->string('password', 60);
+			$table->string('email', 100)->unique('email');
+			$table->string('first_name', 60);
+			$table->string('last_name', 60);
+			$table->boolean('isActive');
+			$table->string('remember_token', 100);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
