dba3ada898531aafef7bd7d42a1aef7e11ece1cb
Roumen Damianoff <roumen@dawebs.com>
2016-01-06 11:17:26 +0300

    added CategoriesTableSeeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('categories')->insert(
+        	[
+	            'name' => 'General',
+	            'slug' => 'general',
+	            'id' => 1
+        	],
+        	[
+	            'name' => 'News',
+	            'slug' => 'news',
+	            'id' => 2
+        	],
+        	[
+	            'name' => 'Reviews',
+	            'slug' => 'reviews',
+	            'id' => 3
+        	],
+        	[
+	            'name' => 'Tutorials',
+	            'slug' => 'tutorials',
+	            'id' => 4
+        	],
+        	[
+	            'name' => 'Videos',
+	            'slug' => 'videos',
+	            'id' => 4
+        	],
+        );
+    }
+}
