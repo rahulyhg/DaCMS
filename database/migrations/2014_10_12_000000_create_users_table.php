510273f415f771d5e832639887fe01bc81f7fdb1
Roumen Damianoff <roumen@dawebs.com>
2016-01-06 11:18:22 +0300

    updated create_users_table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateUsersTable extends Migration {
 
@@ -14,14 +14,18 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::create('users', function(Blueprint $table)
 		{
-			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
+			$table->integer('id', true);
+			$table->string('username', 20)->unique('username');
 			$table->string('password', 60);
+			$table->string('email', 100)->unique('email');
+			$table->string('first_name', 60);
+			$table->string('last_name', 60);
+			$table->boolean('isActive');
 			$table->rememberToken();
 			$table->timestamps();
 		});
 	}
+
 
 	/**
 	 * Reverse the migrations.
