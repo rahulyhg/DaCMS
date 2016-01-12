8f7d981cff63df743a342fcc90c8a3fa015b371d
Roumen Damianoff <roumen@dawebs.com>
2016-01-12 20:20:42 +0200

    added api config

---
+++ config/api.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+'google_analytics' => env('GOOGLE_ANALYTICS'),
+
+'piwik_id' => env('PIWIK_ID'),
+'piwik_domain' => env('PIWIK_DOMAIN'),
+'piwik_url' => env('PIWIK_URL'),
+
+'recaptcha_public' => env('RECAPTCHA_PUB'),
+'recaptcha_private' => env('RECAPTCHA_PRI'),
+
+'disqus_key' => env('DISQUS_KEY'),
+'disqus_forum' => env('DISQUS_FORUM'),
+
+'share_this' => env('SHARETHIS'),
+
+];
