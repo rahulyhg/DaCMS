6a90aded7c6bcc583cea6a49795347529744ae42
Roumen Damianoff <roumen@dawebs.com>
2016-01-14 15:15:14 +0300

    added tinymce.js

---
+++ public/js/tinymce.js
@@ -0,0 +1,24 @@
+tinymce.init({
+    selector: "#post_content",
+    width: 750,
+    height: 250,
+    plugins: [
+      "advlist autolink lists link image charmap print preview anchor",
+      "searchreplace visualblocks code fullscreen",
+      "insertdatetime media table contextmenu paste jbimages"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
+    relative_urls: false
+  });
+tinymce.init({
+  selector: "#resume",
+  width: 750,
+  height: 25,
+  plugins: [
+    "advlist autolink lists link image charmap print preview anchor",
+    "searchreplace visualblocks code fullscreen",
+    "insertdatetime media table contextmenu paste"
+  ],
+  toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
+  relative_urls: false
+});
