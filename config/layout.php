d343b4f80a44863d6712775abcfbe00f1454ef51
Roumen Damianoff <roumen@dawebs.com>
2016-01-14 15:11:14 +0300

    added config\layout

---
+++ config/layout.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+'title' 		=> 'Homepage',
+'description' 	=> 'A very simple CMS build with Laravel and Bootstrap.',
+'keywords' 		=> 'dacms, cms, laravel, bootstrap',
+'robots' 		=> 'all',
+'canonical' 	=> env('APP_URL'),
+'sidebar'		=> true
+
+];
