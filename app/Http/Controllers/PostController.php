115de8b4659ba159725d69407e4c9736da222931
Roumen Damianoff <roumen@dawebs.com>
2016-01-19 15:12:17 +0300

    updated PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,251 +2,104 @@
 
 use App;
 use App\Post;
-use App\Category;
-use App\User;
-use App\Tag;
-use Auth;
 use Asset;
 use Redirect;
-use Validator;
 use Input;
 use Config;
 
 class PostController extends Controller
 {
 
-
 	public function getIndex()
 	{
-		// get current language
-		$lang = $this->getLang();
+		// get latest posts
+		$posts = Post::where('isVisible', '=', '1')->orderBy('created_at', 'desc')->with('tags','categories','author')->paginate(5);
 
-		// meta
-		$meta['title'] = 'Blog';
-		$meta['canonical'] = secure_url('blog');
-		$meta['robots'] = 'index,follow,noodp,noydir';
-		$meta['description'] = 'Blog page';
-		$meta['keywords'] = 'blog';
-
-		// page assets
-		Asset::add(secure_url('js/blog.js'));
-
-		// get data from models
-		$posts = Post::where('isVisible', '=', '1')->orderBy('created_at', 'desc')->paginate(5);
-
-		// check for problems
-		$posts->setPath(secure_url('blog')); // fix pagintor
+		// fix pagination
+		$posts->setPath(secure_url('blog'));
 
 		// get the view
-		return view('post.index')->with('posts', $posts)->with('meta', $meta);
+		return view('post.index', ['posts' => $posts]);
 	}
-
 
 	public function getView($slug)
 	{
-		// get current language
-		$lang = $this->getLang();
-
 		// get needed data from models
-		$post = Post::where('slug', '=', $slug)->where('lang', '=', $lang)->first();
-		$categories = Category::get();
-		$tags = Tag::get();
-		$authors = User::take(10)->get();
+		$post = Post::where('slug', '=', $slug)->with('author','tags','categories')->first();
 
-		// check for problems
-		if (empty($post)) $post = Post::where('slug','=',$slug)->first(); // wrong language
-		if (empty($post)) return abort('404'); // not found
-		if ( ($post->isVisible != 1) && ( (!Auth::user()) || (Auth::user()->id != $post->user_id) ) ) return abort('403'); // draft
+		// if post not found
+		if (empty($post)) return abort('404');
 
-		// meta
-		$meta['title'] = $post->title;
-		$meta['canonical'] = secure_url('blog/'.$post->slug);
-		$meta['robots'] = $post->robots;
-		$meta['description'] = $post->description;
-		$meta['keywords'] = $post->keywords;
-
-		// page assets
-		Asset::add(secure_url('/js/blog.js'));
-
-		if ($post->isDisqus == 1)
-		{
-			Asset::add('https://ws.sharethis.com/button/buttons.js');
-			Asset::addScript('var disqus_config=function(){this.language="'.$lang.'";};','footer');
-			Asset::add(secure_url('js/disqus.js'));
-		}
-		if ($post->isHL == 1)
-		{
-			Asset::add('https://cdn.roumen.it/repo/shl/styles/dark.css');
-			Asset::add('https://cdn.roumen.it/repo/shl/scripts/default.js');
-		}
+		// if post is a draft and user is not logged, is not the author of the post or is not admin
+		if ( ($post->isVisible != 1) && ( (!$this->authUser) || ($this->authUser->id != $post->user_id) ) ) return abort('403');
 
 		// get the view
-		return view('post.view')->with('meta', $meta)->with('post', $post);
+		return view('post.view', ['post' => $post]);
 	}
-
 
 	public function Search()
 	{
-		// get current language
-		$lang = $this->getLang();
-
-		// meta
-		$meta['title'] = 'Search results';
-		$meta['canonical'] = secure_url('search');
-		$meta['robots'] = 'index,follow,noodp,noydir';
-		$meta['description'] = 'Blog page';
-		$meta['keywords'] = 'blog';
-
-		// page assets
-		Asset::add(secure_url('js/blog.js'));
-
 		// get data from models
 		$s = Input::get('s', 'post');
+
+		// get results
 		$posts = Post::where('isVisible', '=', '1')->where('title', 'LIKE', "%$s%")
 												   ->orWhere('content', 'LIKE', "%$s%")
 												   ->orWhere('resume', 'LIKE', "%$s%")
-												   ->orderBy('created_at', 'desc')->paginate(5);
+												   ->orderBy('created_at', 'desc')
+												   ->with('tags','categories','author')
+												   ->paginate(5);
 
-		// check for problems
-		$posts->setPath(secure_url('search')); // fix pagintor
+		// fix pagination
+		$posts->setPath(secure_url('search'));
 
 		// get the view
-		return view('post.search')->with('posts', $posts)->with('s', $s)->with('meta', $meta);
+		return view('post.search', ['posts' => $posts, 's' => $s]);
 	}
 
-
-	public function getEdit($id)
+	public function getUpdate($id)
 	{
+		// get post to edit
 		$post = Post::where('id','=',$id)->first();
 
-		$meta['title'] = 'Edit post';
-
-		$s1  = "$(function(){CKEDITOR.replace('post_content',{toolbar:'Standart',height:'400px',width:'760px',toolbarCanCollapse:true,toolbarStartupExpanded:true,startupShowBorders:true});});";
-		$s1 .= "$(function(){CKEDITOR.replace('resume',{toolbar:'Standart',height:'100px',width:'760px',toolbarCanCollapse:true,toolbarStartupExpanded:true,startupShowBorders:true});});";
-		$s2 = "$('#deleteBtn').click(function(){window.location = '".secure_url('/blog/del/'.$id)."';});";
-
-		Asset::add('https://cdn.roumen.it/repo/ckeditor/4.4.6/basic/ckeditor.js');
-		Asset::addScript($s1, 'ready');
-		Asset::addScript($s2, 'ready');
-
-		return view('post.edit')->with('meta', $meta)->with('post', $post);
+		// get the view
+		return view('post.update', ['post' => $post]);
 	}
 
-
-	public function postEdit($id)
+	public function postUpdate($id)
 	{
-		$rules = [
-				'title'  => 'required|min:3|max:80',
-				'slug' => 'required|min:3',
-				'post_content'  => 'required|min:15',
-				'resume'  => 'required|min:15',
-				'description'  => 'required|min:10|max:180',
-				'keywords' => 'required|min:3|max:90'
-		];
-
-		$validation = Validator::make(Input::all(), $rules);
-
-		if ($validation->passes())
-		{
-				$data = [
-				   'title' => Input::get('title'),
-				   'slug' => Input::get('slug'),
-				   'content' => Input::get('post_content'),
-				   'resume' => Input::get('resume'),
-				   'description' => Input::get('description'),
-				   'keywords' => Input::get('keywords'),
-				   'robots' => Input::get('robots'),
-				   'lang' => Input::get('lang'),
-				   'isVisible' => Input::get('visible'),
-				   'isHL' => Input::get('hl'),
-				   'isDisqus' => Input::get('comments'),
-				];
-
-		 Post::where('id','=',$id)->update($data);
-
-		 return Redirect::secure('blog/'.Input::get('slug'));
-
-		}
-
-		return Redirect::secure('blog/edit/'.$id)->withErrors($validation);
+		// update post
+		return Post::updatePost($id, Input::all());
 	}
-
 
 	public function getCreate()
 	{
-		$meta['title'] = "Create post";
-
-		$s1  = "$(function(){CKEDITOR.replace('post_content',{toolbar:'Standart',height:'400px',width:'760px',toolbarCanCollapse:true,toolbarStartupExpanded:true,startupShowBorders:true});});";
-		$s1 .= "$(function(){CKEDITOR.replace('resume',{toolbar:'Standart',height:'100px',width:'760px',toolbarCanCollapse:true,toolbarStartupExpanded:true,startupShowBorders:true});});";
-
-		Asset::add('https://cdn.roumen.it/repo/ckeditor/4.4.6/basic/ckeditor.js');
-		Asset::addScript($s1, 'ready');
-
-		return view('post.create')->with('meta', $meta);
+		// get the view
+		return view('post.create');
 	}
-
 
 	public function postCreate()
 	{
-		$rules = array(
-				'title'  => 'required|min:3|max:80',
-				'slug' => 'required|min:3',
-				'post_content'  => 'required|min:15',
-				'resume'  => 'required|min:15',
-				'description'  => 'required|min:10|max:180',
-				'keywords' => 'required|min:3|max:90'
-		);
-
-		$validation = Validator::make(Input::all(), $rules);
-
-		if ($validation->passes())
-		{
-				$data = [
-				   'title' => Input::get('title'),
-				   'slug' => Input::get('slug'),
-				   'content' => Input::get('post_content'),
-				   'resume' => Input::get('resume'),
-				   'description' => Input::get('description'),
-				   'keywords' => Input::get('keywords'),
-				   'robots' => Input::get('robots'),
-				   'lang' => Input::get('lang'),
-				   'isVisible' => Input::get('visible'),
-				   'isHL' => Input::get('hl'),
-				   'isDisqus' => Input::get('comments'),
-				   'user_id' => Auth::user()->id
-				];
-
-		 Post::insert($data);
-
-		 return Redirect::secure('/blog/'.Input::get('slug'));
-
-		}
-
-		return Redirect::secure('blog/add')->withErrors($validation);
+		// create new post
+		return Post::createPost(Input::all());
 	}
-
 
 	public function getDelete($id)
 	{
+		// get the post to delete
 		$post = Post::where('id','=',$id)->first();
 
-		$meta['title'] = 'DELETE: ' . $post->title;
+		// if post is missing redirect to blog
+		if (empty($post)) Redirect::secure('blog');
 
-		return view('post.delete')->with('meta', $meta)->with('post', $post);
+		// get the view
+		return view('post.delete', ['post' => $post]);
 	}
-
 
 	public function postDelete($id)
 	{
-		  if (Input::get('confirm')==true)
-		  {
-			  Post::where('id','=',$id)->delete();
-
-			  return Redirect::secure('/blog');
-		  }
-
-		  return Redirect::secure('blog/del/'.$id);
+	  	// delete post
+	  	return Post::deletePost($id, Input::all());
 	}
-
 
 }
