43bc106018feeb211978f5a296ecb6bbcc608904
Roumen Damianoff <roumen@dawebs.com>
2016-01-25 00:09:33 +0200

    updated CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,125 +13,62 @@ use Config;
 class CategoryController extends Controller
 {
 
-
 	public function getView($slug)
 	{
-
 		// get needed data from models
-		$category = Category::where('slug', '=', $slug)->first();
-		$categories = Category::get();
-		$authors = User::take(10)->get();
+		$category = Category::where('slug', '=', $slug)->with('posts')->first();
 
-		// check for problems
-		if (empty($category)) return abort('404'); // not found
-
-		// meta
-		$meta['title'] = "Category: " . $category->name;
-		$meta['canonical'] = secure_url('category/'.$category->slug);
-		$meta['description'] = 'Posts from category ' . $category->name;;
-		$meta['keywords'] = 'posts, category, ' . $category->name;
-
-		// page assets
-		Asset::add(secure_url('js/blog.js'));
+		// not found
+		if (empty($category)) return abort('404');
 
 		// get the view
-		return view('category.view')->with('meta', $meta)->with('category', $category)->with('categories', $categories)->with('authors', $authors);
+		return view('category.view', ['category' => $category]);
 	}
 
-
-	public function getEdit($id)
+	public function getUpdate($id)
 	{
+		// get category to update
 		$category = Category::where('id','=',$id)->first();
 
-		$meta['title'] = 'Edit category';
+		// not found
+		if (empty($category)) return abort('404');
 
-		$s1 = "$('#deleteBtn').click(function(){window.location = '".secure_url('/category/del/'.$id)."';});";
-
-		Asset::addScript($s1, 'ready');
-
-		return view('category.edit')->with('meta', $meta)->with('category', $category);
+		// get the view
+		return view('category.update', ['category' => $category]);
 	}
 
-
-	public function postEdit($id)
+	public function postUpdate($id)
 	{
-		$rules = array(
-				'name'  => 'required|min:2|max:80',
-				'slug' => 'required|min:2'
-		);
-
-		$validation = Validator::make(Input::all(), $rules);
-
-		if ($validation->passes())
-		{
-				$data = array(
-				   'name' => Input::get('name'),
-				   'slug' => Input::get('slug')
-				);
-
-		 Category::where('id','=',$id)->update($data);
-
-		}
-
-		return Redirect::secure('category/edit/'.$id)->withErrors($validation);
+		// update categorory
+		return Category::updateCategory($id, Input::all());
 	}
-
 
 	public function getCreate()
 	{
-		$meta['title'] = "Create category";
-
-		return view('category.create')->with('meta', $meta);
+		// get the view
+		return view('category.create');
 	}
-
 
 	public function postCreate()
 	{
-		$rules = [
-			'name'  => 'required|min:2|max:80',
-			'slug' => 'required|min:2'
-		];
-
-		$validation = Validator::make(Input::all(), $rules);
-
-		if ($validation->passes())
-		{
-			$data = [
-				'name' => Input::get('name'),
-				'slug' => Input::get('slug')
-			];
-
-			Category::insert($data);
-
-			return Redirect::secure('/category/'.Input::get('slug'));
-
-		}
-
-		return Redirect::secure('category/add')->withErrors($validation);
+		// create category
+		return Category::createCategory(Input::all());
 	}
-
 
 	public function getDelete($id)
 	{
 		$category = Category::where('id','=',$id)->first();
 
-		$meta['title'] = 'DELETE: ' . $category->name;
+		// not found
+		if (empty($category)) return abort('404');
 
-		return view('category.delete')->with('meta', $meta)->with('category', $category);
+		return view('category.delete', ['category' => $category]);
 	}
-
 
 	public function postDelete($id)
 	{
-		if (Input::get('confirm')==true)
-		{
-		  Category::where('id','=',$id)->delete();
-
-		  return Redirect::secure('/');
-		}
-
-		return Redirect::secure('category/del/'.$id);
+		// delete category
+		return Category::deleteCategory($id, Input::all());
 	}
-
 
 }
