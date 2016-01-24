96d846613c1f13df24d3e3e57fa2c95e96d60ef5
Roumen Damianoff <roumen@dawebs.com>
2016-01-25 01:33:55 +0200

    wip jbimages

--- public/tinymce/4.3.3/plugins/jbimages/is_allowed.php
+++ public/tinymce/4.3.3/plugins/jbimages/is_allowed.php
@@ -11,30 +11,30 @@
  *
  * Version: 2.3 released 23/06/2013
  */
- 
- 
+
+
 /*-------------------------------------------------------------------
 |
 | IMPORTANT NOTE! In case, when TinyMCE’s folder is not protected with HTTP Authorisation,
-| you should require is_allowed() function to return 
+| you should require is_allowed() function to return
 | `TRUE` if user is authorised,
 | `FALSE` - otherwise
-| 
+|
 |  This is intended to protect upload script, if someone guesses it's url.
-| 
+|
 -------------------------------------------------------------------*/
 
 function is_allowed()
 {
 	global $_COOKIE, $_SERVER;
- 
+
 	//$ip = $_SERVER["REMOTE_ADDR"];
- 
-	if (isset($_COOKIE["laravel_session"]))
+
+	if (isset($_COOKIE["dacms_session"]))
 	{
 		return TRUE;
 	}
- 
+
 	return FALSE;
 }
 
